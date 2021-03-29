0e56ef37c0aee10a3b9932b8b809bbc20fa11dfb
Ellie Su <ellie.su@goldenf.co>
2021-03-29 16:38:23 +0800

    add odd type

--- src/Contract/WMLIVEServiceInterface.php
+++ src/Contract/WMLIVEServiceInterface.php
@@ -14,9 +14,10 @@ interface WMLIVEServiceInterface {
      * @param string $game_code
      * @param string $language
      * @param string $back_url
+     * @param string $odd_type
      * @return mixed
      */
-    function gameLaunch(string $op_code, string $account, string $game_code, string $language, string $back_url = null);
+    function gameLaunch(string $op_code, string $account, string $game_code, string $language, string $back_url = null, string $odd_type = null);
 
     /**
      * 遊戲詳情
